ceff33417f952dfb82fe13e9cb25e8df0f18c047
gheeeeeeee <gheoputra55@gmail.com>
2024-04-22 15:19:49 +0700

    memasukkan cin dan cout di prosedur input

--- OOP1_TIB_0058/OOP1_TIB_0058.cpp
+++ OOP1_TIB_0058/OOP1_TIB_0058.cpp
@@ -23,7 +23,13 @@ private:
     string kodeMK;
     string namaMK;
     int sks;
-    void input(){
+    void input() {
+        cout << "Kode MK: ";
+        cin >> kodeMK;
+        cout << "Nama MK: ";
+        cin >> namaMK;
+        cout << "SKS: ";
+        cin >> sks;
     }
     void output() {
 
